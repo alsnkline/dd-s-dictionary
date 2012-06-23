0a97b61dbaf67045f16db2fe6c0fa531241844fd
Alison Kline <alison@homeMac>
2012-06-23 09:50:05 -0700

    Initial Commit

---
+++ DDPrototype/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DDPrototype
+//
+//  Created by Alison Kline on 6/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
