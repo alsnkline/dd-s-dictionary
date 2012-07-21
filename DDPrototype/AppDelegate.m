2192a1dd57771691b73600a4a7da9e9d00ff3784
Alison Kline <alison@homeMac>
2012-07-21 14:40:33 -0700

    cleaning out the phonemes (will add back if when we add that feature) removing the commented out code from appDelegate

--- DDPrototype/AppDelegate.m
+++ DDPrototype/AppDelegate.m
@@ -21,86 +21,9 @@
 {
     // Override point for customization after application launch.
     
-    //see if there are any dictionary's
-//    NSArray *dictionariesAvailable = [DictionaryHelper currentContentsOfdictionaryDirectory];
-//    NSLog(@"dictionariesAvailable = %@", dictionariesAvailable);
-//    if ([dictionariesAvailable count] == 1) {
-//        NSURL *dictionaryURL = [dictionariesAvailable lastObject];
-//        NSString *activeDictionaryName = [dictionaryURL lastPathComponent];
-//        NSLog(@"Opening the 1 dicitonary available its name: %@", activeDictionaryName);
-//        [DictionarySetupViewController loadDictionarywithName:activeDictionaryName passAroundIn:self.window.rootViewController] ;
-//    }
-    //else {
-//        //Get scource file for words to populate dictionary -
-//        NSBundle *dictionaryShippingWithApp = [DictionaryHelper defaultDictionaryBundle];
-//
-//        NSError *error = nil;
-//        GDataXMLDocument *XMLdoc = [GDataXMLNodeHelper loadDictionaryFromXMLInDictionaryBundle:dictionaryShippingWithApp Error:&error];
-//        // GDataXMLDocument *doc = [GDataXMLNodeHelper loadDictionaryFromXMLError:&error];
-//
-//        if (error) {
-//            UIAlertView *alertUser = [[UIAlertView alloc] initWithTitle:@"Dictionary XML parsing" 
-//                                                                message:[NSString stringWithFormat:@"It seems we can't read your XML Dictionary. Please confirm it conforms to the expected xml format (%@)", error] 
-//                                                               delegate:self cancelButtonTitle:@"OK" 
-//                                                      otherButtonTitles:nil];
-//            NSLog(@"error %@ %@",error, [error userInfo]);
-//            [alertUser sizeToFit];
-//            [alertUser show];
-//        }
-//        if (XMLdoc) {
-//            [self showExplanationForFrozenUI];
-//            
-//            NSString *dictionaryName = [GDataXMLNodeHelper dictionaryNameFor:@"bundleName" FromXMLDoc:XMLdoc];
-//            
-//            //Get UIManagedDocument for dictionary
-//            [DictionaryHelper openDictionary:dictionaryName usingBlock:^ (UIManagedDocument *dictionaryDatabase) {
-//                NSLog(@"Got dictionary %@", [dictionaryDatabase.fileURL lastPathComponent]);
-//                
-//                //process file to populate UIManagedDocument - need to add way to force reanalysis for changes
-//                [GDataXMLNodeHelper processXMLfile:XMLdoc intoManagedObjectContext:dictionaryDatabase.managedObjectContext];
-//                
-//                //share activeDictionary with all VC's
-//                [DictionaryHelper passActiveDictionary:dictionaryDatabase arroundVCsIn:self.window.rootViewController];
-//            }];
-//        }
-//
-//    }
-    return YES;
+       return YES;
 }
-    
-//- (void)loadDictionarywithName:(NSString *)dictionaryName createFromXML:(GDataXMLDocument *)XMLdoc
-//{
-//    [DictionaryHelper openDictionary:dictionaryName usingBlock:^ (UIManagedDocument *dictionaryDatabase) {
-//        
-//        NSLog(@"Got dictionary %@ doc state = %@", [dictionaryDatabase.fileURL lastPathComponent], [DictionaryHelper stringForState:dictionaryDatabase.documentState]);
-//        if (dictionaryDatabase.documentState == UIDocumentStateNormal) {
-//            
-//            if (XMLdoc) {
-//                
-//                //process file to populate and save the UIManagedDocument
-//                [GDataXMLNodeHelper processXMLfile:XMLdoc intoManagedObjectContext:dictionaryDatabase.managedObjectContext];
-//                [DictionaryHelper numberOfWordsInCoreDataDocument:dictionaryDatabase];
-//            }
-//            
-//            //share activeDictionary with all VC's
-//            [DictionaryHelper passActiveDictionary:dictionaryDatabase arroundVCsIn:self.window.rootViewController];
-//            
-//        } else {
-//            NSLog(@"dictionary documentState NOT normal");
-//        }
-//    }];
-//}
-//
-//- (void) showExplanationForFrozenUI
-//{
-//    UIAlertView *alertUser = [[UIAlertView alloc] initWithTitle:@"Dictionary processing" 
-//                                                        message:[NSString stringWithFormat:@"Please wait while we build your dictionary for the first time."] 
-//                                                       delegate:self cancelButtonTitle:@"OK" 
-//                                              otherButtonTitles:nil];
-//    [alertUser sizeToFit];
-//    [alertUser show];
-//}
-							
+    							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
